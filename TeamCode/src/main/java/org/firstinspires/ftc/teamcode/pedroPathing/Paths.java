70587384c7064efe88efc46bca57b444ed5e694b
anaoanao1 <yufei8028@gmail.com>
2025-11-07 14:59:24 -0800

    Basic Pedro path

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Paths.java
@@ -0,0 +1,60 @@
+package org.firstinspires.ftc.teamcode.pedroPathing;
+
+import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.BezierCurve;
+import com.pedropathing.geometry.BezierLine;
+import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.PathChain;
+
+public class Paths { // supposed to be static?
+// Basic Path
+    public PathChain Path1, Path2; // 1 = red, 2 = blue, start at back, move up to shoot
+    public PathChain Path3, Path4; // 3 = red, 4 = blue, start at top, move down to shoot
+    private double shootingPositionX, shootingPositionY;
+
+    public Paths(Follower follower) {
+        shootingPositionX = 96;
+        shootingPositionY = 96;
+        Path1 = follower
+                .pathBuilder()
+                .addPath(
+                        new BezierCurve(
+                                new Pose(56.000, 8.000),
+                                new Pose(56.000, 36.000),
+                                new Pose(shootingPositionX, shootingPositionY)
+                        )
+                )
+                .setLinearHeadingInterpolation(Math.toRadians(90), Math.toRadians(45))
+                .build();
+
+        Path2 = follower
+                .pathBuilder()
+                .addPath(
+                        new BezierCurve(
+                                new Pose(88.000, 8.000),
+                                new Pose(88.000, 36.000),
+                                new Pose(144 - shootingPositionX, shootingPositionY)
+                        )
+                )
+                .setLinearHeadingInterpolation(Math.toRadians(90), Math.toRadians(135))
+                .build();
+
+        Path3 = follower
+                .pathBuilder()
+                .addPath(
+                        new BezierLine(new Pose(110.000, 136.000),
+                                new Pose(shootingPositionX, shootingPositionY))
+                )
+                .setLinearHeadingInterpolation(Math.toRadians(90), Math.toRadians(45))
+                .build();
+
+        Path4 = follower
+                .pathBuilder()
+                .addPath(
+                        new BezierLine(new Pose(34.000, 136.000),
+                                new Pose(144 - shootingPositionX, shootingPositionY))
+                )
+                .setLinearHeadingInterpolation(Math.toRadians(90), Math.toRadians(135))
+                .build();
+    }
+}
