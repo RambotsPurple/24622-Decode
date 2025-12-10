f86ef96fe7eac93adaf09c718b6df7e5eb77b1d9
anaoanao1 <yufei8028@gmail.com>
2025-12-09 16:06:37 -0800

    Paths

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/config/Paths/Paths.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/config/Paths/Paths.java
@@ -16,18 +16,18 @@ public class Paths { // supposed to be static?
     shooting position = (64, 16) at 113.5 degrees
      */
     public PathChain startToShoot; // move from start to shoot 3 balls
-    public PathChain intakeRow1A, intakeRow1B, intakeRow1C; // starting from shooting pos, intake row 1 of balls and move back to shoot
-    public PathChain intakeRow2A, intakeRow2B, intakeRow2C; // starting from shooting pos, intake row 2 of balls and move back to shoot
+    public PathChain intakeRow1A, intakeRow1B, intakeRow1C; // starting from shooting pos, intake 2 balls in row 1 and move back to shoot
+    public PathChain intakeRow2A, intakeRow2B, intakeRow2C; // starting from shooting pos, intake 2 balls in row 2 and move back to shoot
+    public PathChain intakeRow3A, intakeRow3B, intakeRow3C; // starting from shooting pos, intake 2 balls in row 3 and move back to shoot
+    public PathChain intakeColumnA, intakeColumnB, intakeColumnC;
+
     private Pose shootingPose;
 
     public Paths(Follower follower) {
-
-        shootingPose = new Pose(64.000, 16.000);
-
         startToShoot = follower
                 .pathBuilder()
                 .addPath(
-                        new BezierLine(new Pose(56.000, 9.000), shootingPose)
+                        new BezierLine(new Pose(56.000, 9.000), new Pose(64.000, 16.000))
                 )
                 .setLinearHeadingInterpolation(Math.toRadians(90), Math.toRadians(113.5))
                 .build();
@@ -35,7 +35,7 @@ public class Paths { // supposed to be static?
         intakeRow1A = follower
                 .pathBuilder()
                 .addPath(
-                        new BezierLine(shootingPose, new Pose(45.000, 36.000))
+                        new BezierLine(new Pose(64.000, 16.000), new Pose(45.000, 36.000))
                 )
                 .setLinearHeadingInterpolation(Math.toRadians(113.5), Math.toRadians(180))
                 .build();
@@ -43,7 +43,7 @@ public class Paths { // supposed to be static?
         intakeRow1B = follower
                 .pathBuilder()
                 .addPath(
-                        new BezierLine(new Pose(45.000, 36.000), new Pose(16.000, 36.000))
+                        new BezierLine(new Pose(45.000, 36.000), new Pose(31.000, 36.000))
                 )
                 .setTangentHeadingInterpolation()
                 .build();
@@ -51,7 +51,7 @@ public class Paths { // supposed to be static?
         intakeRow1C = follower
                 .pathBuilder()
                 .addPath(
-                        new BezierLine(new Pose(16.000, 36.000), shootingPose)
+                        new BezierLine(new Pose(31.000, 36.000), new Pose(64.000, 16.000))
                 )
                 .setLinearHeadingInterpolation(Math.toRadians(180), Math.toRadians(113.5))
                 .build();
@@ -59,7 +59,7 @@ public class Paths { // supposed to be static?
         intakeRow2A = follower
                 .pathBuilder()
                 .addPath(
-                        new BezierLine(shootingPose, new Pose(45.000, 60.000))
+                        new BezierLine(new Pose(64.000, 16.000), new Pose(45.000, 60.000))
                 )
                 .setLinearHeadingInterpolation(Math.toRadians(113.5), Math.toRadians(180))
                 .build();
@@ -67,7 +67,7 @@ public class Paths { // supposed to be static?
         intakeRow2B = follower
                 .pathBuilder()
                 .addPath(
-                        new BezierLine(new Pose(45.000, 60.000), new Pose(16.000, 60.000))
+                        new BezierLine(new Pose(45.000, 60.000), new Pose(31.000, 60.000))
                 )
                 .setTangentHeadingInterpolation()
                 .build();
@@ -75,7 +75,55 @@ public class Paths { // supposed to be static?
         intakeRow2C = follower
                 .pathBuilder()
                 .addPath(
-                        new BezierLine(new Pose(16.000, 60.000), shootingPose)
+                        new BezierLine(new Pose(31.000, 60.000), new Pose(64.000, 16.000))
+                )
+                .setLinearHeadingInterpolation(Math.toRadians(180), Math.toRadians(113.5))
+                .build();
+
+        intakeRow3A = follower
+                .pathBuilder()
+                .addPath(
+                        new BezierLine(new Pose(64.000, 16.000), new Pose(45.000, 84.000))
+                )
+                .setLinearHeadingInterpolation(Math.toRadians(113.5), Math.toRadians(180))
+                .build();
+
+        intakeRow3B = follower
+                .pathBuilder()
+                .addPath(
+                        new BezierLine(new Pose(45.000, 84.000), new Pose(31.000, 84.000))
+                )
+                .setTangentHeadingInterpolation()
+                .build();
+
+        intakeRow3C = follower
+                .pathBuilder()
+                .addPath(
+                        new BezierLine(new Pose(31.000, 84.000), new Pose(64.000, 16.000))
+                )
+                .setLinearHeadingInterpolation(Math.toRadians(180), Math.toRadians(113.5))
+                .build();
+
+        intakeColumnA = follower
+                .pathBuilder()
+                .addPath(
+                        new BezierLine(new Pose(64.000, 16.000), new Pose(19.000, 22.000))
+                )
+                .setLinearHeadingInterpolation(Math.toRadians(113.5), Math.toRadians(90))
+                .build();
+
+        intakeColumnB = follower
+                .pathBuilder()
+                .addPath(
+                        new BezierLine(new Pose(19.000, 22.000), new Pose(19.000, 55.000))
+                )
+                .setTangentHeadingInterpolation()
+                .build();
+
+        intakeColumnC = follower
+                .pathBuilder()
+                .addPath(
+                        new BezierLine(new Pose(19.000, 55.000), new Pose(64.000, 16.000))
                 )
                 .setLinearHeadingInterpolation(Math.toRadians(180), Math.toRadians(113.5))
                 .build();
