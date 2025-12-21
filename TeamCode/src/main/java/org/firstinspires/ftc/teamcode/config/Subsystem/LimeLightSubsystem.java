54c24caa3390bfbde9c7ec864705066945161a39
pokiji <110128494+Pokiji@users.noreply.github.com>
2025-12-21 12:52:08 -0800

    ll

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/config/Subsystem/LimeLightSubsystem.java
@@ -0,0 +1,74 @@
+package org.firstinspires.ftc.teamcode.config.Subsystem;
+
+import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.qualcomm.hardware.limelightvision.LLResultTypes.*;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
+import org.firstinspires.ftc.teamcode.config.Util.Alliance;
+
+import java.util.List;
+
+public class LimeLightSubsystem extends SubsystemBase {
+    private final Limelight3A limelight;
+    private Alliance alliance;
+
+    private final int id;
+    public  LimeLightSubsystem(HardwareMap hw, Alliance alliance){
+        limelight = hw.get(Limelight3A.class, "limelight");
+        limelight.setPollRateHz(100);
+        limelight.pipelineSwitch(0);
+        this.alliance = alliance;
+        id = this.alliance == Alliance.RED?24:20;
+    }//end of constructor
+
+    public void lStart(){
+        limelight.start();
+    }//end of lStart
+
+    public double getDist(){
+        FiducialResult tag = null;
+        double tagDist = 0;
+        List<FiducialResult> results = limelight.getLatestResult().getFiducialResults();
+
+        if (results.isEmpty()) return 0;
+
+        for (FiducialResult fiducial : results) {
+            if (fiducial!=null &&fiducial.getFiducialId() == id){
+                tag = fiducial;
+                break;
+            }//end of if
+        }//end of for
+
+        if(tag !=null){
+            tagDist = (tag.getCameraPoseTargetSpace().getPosition().z/ DistanceUnit.mPerInch);
+        }//end of if
+        return tagDist;
+
+    }//end getDist
+
+    public double getHoriError(){
+        FiducialResult tag = null;
+        double tagYaw = 0;
+        List<FiducialResult> results = limelight.getLatestResult().getFiducialResults();
+
+        if (results.isEmpty()) return 0;
+
+        for (FiducialResult fiducial : results) {
+            if (fiducial!=null &&fiducial.getFiducialId() == id){
+                tag = fiducial;
+                break;
+            }//end of if
+        }//end of for
+
+        if(tag !=null){
+            tagYaw = tag.getTargetXDegrees();
+        }//end of if
+
+        return tagYaw;
+    }//end of getHoriError
+
+
+}//end of LimeLightSubsystem
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/CloseBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/CloseBlue.java
@@ -15,34 +15,13 @@ public class CloseBlue extends OpModeCommand {
     Robot robot;
 
     @Override
-    public void init() {
-        robot = new Robot(hardwareMap, Alliance.BLUE, telemetry);
-        if(gamepad1.xWasPressed()){
-            robot.alliance = Alliance.BLUE;
-
-        }else if(gamepad1.bWasPressed()){
-            robot.alliance = Alliance.RED;
-        }//end of else if
-
-        telemetry.addLine("press X to change to BLUE Alliance");
-        telemetry.addLine("press B to change to RED Alliance");
-        telemetry.addData("current alliance selected:", robot.alliance);
-        telemetry.update();
-        robot.tele();
-    }
-
-    @Override
-    public void start(){
-        robot.aStart();
-    }
-
-    @Override
-    public void loop() {
-        robot.aPeriodic();
-    }
-
-    @Override
     public void initialize() {
+        robot = new Robot(hardwareMap, Alliance.BLUE, telemetry);
+            schedule(
 
+
+        );
     }
+
+
 }
