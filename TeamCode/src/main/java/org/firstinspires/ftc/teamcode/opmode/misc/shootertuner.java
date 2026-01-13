1c6ce6ecb639f74f41304475bb3f175ca4b62078
Alex <110128494+Pokiji@users.noreply.github.com>
2026-01-13 11:27:12 -0800

    i cant read

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/misc/shootertuner.java
@@ -0,0 +1,96 @@
+package org.firstinspires.ftc.teamcode.opmode.misc;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.PIDFCoefficients;
+
+import org.firstinspires.ftc.teamcode.config.Robot;
+import org.firstinspires.ftc.teamcode.config.Subsystem.ShooterSubsystem;
+import org.firstinspires.ftc.teamcode.config.Util.Alliance;
+@TeleOp(name = "shooterTuner")
+public class shootertuner extends OpMode {
+    public DcMotorEx shooter1, shooter2;
+    double highvel = 6000;
+    double lowvel = 1300;
+    double currvelt = highvel;
+    double p = 0;
+    double f=0;
+    double[] stepSizes = {10,1,.1,.001,.0001};
+    int stepindex = 1;
+    @Override
+    public void init() {
+        shooter1 = hardwareMap.get(DcMotorEx.class, "s1");
+        shooter2 = hardwareMap.get(DcMotorEx.class, "s2");
+
+        shooter1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        shooter2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        shooter1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        //issue with shooter2 when encoder cable not connected
+        shooter2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        // Orientation for shooter
+        shooter1.setDirection(DcMotor.Direction.FORWARD);
+        shooter2.setDirection(DcMotor.Direction.FORWARD);
+        PIDFCoefficients pidfCoefficients = new PIDFCoefficients(p,0,0,f);
+        shooter1.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, pidfCoefficients);
+        shooter2.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, pidfCoefficients);
+
+
+    } // init
+
+
+
+    @Override
+    public void start(){
+
+    }
+
+    @Override
+    public void loop() {
+        if(gamepad1.yWasPressed()){
+            if(currvelt == highvel){
+                currvelt = lowvel;
+            }else{
+                currvelt = highvel;
+            }
+        }if (gamepad1.bWasPressed()){
+            stepindex = (stepindex +1)% stepSizes.length;
+        }
+        if(gamepad1.dpadUpWasPressed()){
+            p += stepSizes[stepindex];
+        }
+        if(gamepad1.dpadDownWasPressed()){
+            p-= stepSizes[stepindex];
+        }
+        if(gamepad1.dpadLeftWasPressed()){
+            f+= stepSizes[stepindex];
+        }
+        if(gamepad1.dpadRightWasPressed()){
+            f-= stepSizes[stepindex];
+        }
+
+        PIDFCoefficients pidfCoefficients = new PIDFCoefficients(p,0,0,f);
+        shooter1.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, pidfCoefficients);
+        shooter2.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, pidfCoefficients);
+
+        shooter1.setVelocity(currvelt);
+        shooter2.setVelocity(currvelt);
+
+        double currvel =  shooter1.getVelocity();
+        double error  = currvelt - currvel;
+        telemetry.addData("targ vel:",currvelt);
+        telemetry.addData("curr vel","%.2f", currvel);
+        telemetry.addData("error", error);
+        telemetry.addData("p", p);
+        telemetry.addData("f", f);
+        telemetry.addData("index", stepindex);
+        telemetry.update();
+
+    }
+}
